62a068ef3ed2a775eee0a417f97ebf5d313e00f2
ZLoganZ <ln26805@gmail.com>
2024-05-30 15:35:52 +0700

    [UPDATE]: Update prettier configuration and theme colors

--- libs/theme.ts
+++ libs/theme.ts
@@ -2,4 +2,7 @@ import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
   /** Put your mantine theme override here */
+  cursorType: 'pointer',
+  black: 'hsl(222.2 84% 4.9%)',
+  white: 'hsl(0 0% 100%)'
 });
--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -2,7 +2,7 @@ import { AuthenticationForm } from '@/components/pages/auth/AuthenticationForm';
 
 export default function Auth() {
   return (
-    <div className='flex-center h-dvh w-full'>
+    <div className='bg-background flex-center h-dvh w-full'>
       <AuthenticationForm />
     </div>
   );
