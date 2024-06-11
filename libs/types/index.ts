f4e50dff1835bdae3d02cc38f2d220386bc990ad
ZLoganZ <ln26805@gmail.com>
2024-06-11 16:33:41 +0700

    [INIT]: Update next.config to disable react strict mode and add SocketStore

--- libs/types/index.ts
+++ libs/types/index.ts
@@ -1,1 +1,7 @@
 // for all type
+export enum NotificationType {
+  SETUP_WORKSPACE = 'setup_workspace',
+  CREATE_NORMAL = 'create_normal_notification',
+  CREATE_WORKSPACE = 'create_workspace_notification',
+  NEW = 'new_notification'
+}
