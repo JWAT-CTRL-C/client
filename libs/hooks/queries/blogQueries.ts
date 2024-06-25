6c14293d7b51d15f4ed73c9d6416e699edb4ee1e
quangvinht <96629186+quangvinht@users.noreply.github.com>
2024-06-25 14:13:10 +0700

    [REFACTOR]: add enabled in blog queries

--- libs/hooks/queries/blogQueries.ts
+++ libs/hooks/queries/blogQueries.ts
@@ -14,6 +14,9 @@ import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { useDebouncedValue } from '@mantine/hooks';
 import { BlogQueryEnum } from '@/libs/constants/queryKeys/blog';
 import { workspacesType } from '@/libs/types/workspacesType';
+import { getUserAuth } from '@/libs/utils';
+
+const userInfo = getUserAuth();
 
 export const useFetchBlogs = () => {
   return useInfiniteQuery({
@@ -21,28 +24,32 @@ export const useFetchBlogs = () => {
     queryFn: ({ pageParam }) => fetchBlogs(pageParam),
     initialPageParam: 1,
     getNextPageParam: (lastPage, _, lastPageParam) => (lastPage?.length < 12 ? null : lastPageParam + 1),
-    select: (data) => data.pages.flat()
+    select: (data) => data.pages.flat(),
+    enabled: !!userInfo.access_token
   });
 };
 
 export const useFetchRecentBlogs = () => {
   return useQuery<BlogResponse[]>({
     queryKey: [BlogQueryEnum.BLOGS_RECENT],
-    queryFn: () => fetchRecentBlogs()
+    queryFn: () => fetchRecentBlogs(),
+    enabled: !!userInfo.access_token
   });
 };
 
 export const useFetchBlogById = (blog_id: string) => {
   return useQuery<BlogResponse>({
     queryKey: [BlogQueryEnum.BLOGS, blog_id],
-    queryFn: () => fetchBlogById(blog_id)
+    queryFn: () => fetchBlogById(blog_id),
+    enabled: !!blog_id
   });
 };
 
 export const useFetchBlogsCurrentUser = () => {
   return useQuery<BlogResponse[]>({
     queryKey: [BlogQueryEnum.BLOGS_CURRENT_USER],
-    queryFn: () => fetchBlogsForCurrentUser()
+    queryFn: () => fetchBlogsForCurrentUser(),
+    enabled: !!userInfo.access_token
   });
 };
 
@@ -59,13 +66,15 @@ export const useFetchBlogsCurrentUserByTitle = (blog_tle: string) => {
 export const useFetchRelatedBlog = (blog_id: string) => {
   return useQuery<BlogResponse[]>({
     queryKey: [BlogQueryEnum.BLOGS_RELATED, blog_id],
-    queryFn: () => fetchRelatedBlogs(blog_id)
+    queryFn: () => fetchRelatedBlogs(blog_id),
+    enabled: !!blog_id
   });
 };
 
 export const useFetchWorkSpaceInfo = () => {
   return useQuery<Pick<workspacesType, 'wksp_id' | 'wksp_name' | 'resources'>[]>({
     queryKey: [BlogQueryEnum.BLOGS_WORKSPACES_INFO],
-    queryFn: () => fetchWorkspacesInfo()
+    queryFn: () => fetchWorkspacesInfo(),
+    enabled: !!userInfo.access_token
   });
 };
