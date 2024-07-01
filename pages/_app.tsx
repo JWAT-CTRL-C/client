d1e1c88d59bc18e7a7afc67e1a1506a3611db6ed
ZLoganZ <ln26805@gmail.com>
2024-07-01 17:30:55 +0700

    chore: Update refetchOnMount option to 'always' in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,7 +40,7 @@ export default function App({
         defaultOptions: {
           queries: {
             refetchOnWindowFocus: false,
-            refetchOnMount: true,
+            refetchOnMount: 'always',
             retry: 1,
             staleTime: 1000 * 60 * 10
           }
