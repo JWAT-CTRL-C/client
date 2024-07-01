eaa6a5312223259a4ce82eeb921395003405fd05
ZLoganZ <ln26805@gmail.com>
2024-07-01 12:42:27 +0700

    [REFACTOR]: Refactor ToTopButton component to add zIndex to Affix position

--- libs/hooks/mutations/userMutations.ts
+++ libs/hooks/mutations/userMutations.ts
@@ -1,6 +1,14 @@
 import { GET_ALL_USERS_FOR_ADMIN_KEY, MY_INFO_KEY } from '@/libs/constants/queryKeys/user';
 import { User, UserForm, UserFormForAdmin } from '@/libs/types/userType';
-import { uploadImage, updateUser, removeUser, restoreUser, createUser } from '@/services/userServices';
+import {
+  uploadImage,
+  updateUser,
+  removeUser,
+  restoreUser,
+  createUser,
+  resetPassword,
+  changePassword
+} from '@/services/userServices';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const useUploadImage = () => {
@@ -75,15 +83,51 @@ export const useRestoreUser = () => {
 
 export const useCreateUser = () => {
   const queryClient = useQueryClient();
-  const mutation = useMutation<void, Error, Omit<UserFormForAdmin, 'user_id'>>({
+  const mutation = useMutation<void, Error, Omit<UserFormForAdmin, 'user_id' | 'pass'>>({
     mutationFn: createUser,
-    onSuccess: async (_data, variable) => {
+    onSuccess: async (_data) => {
       await queryClient.invalidateQueries({ queryKey: [GET_ALL_USERS_FOR_ADMIN_KEY] });
     }
   });
 
   return {
     createUser: mutation.mutate,
+    isPending: mutation.isPending,
+    isError: mutation.isError
+  };
+};
+
+export const useResetPassword = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<void, Error, number>({
+    mutationFn: async (user_id: number) => {
+      await resetPassword(user_id);
+    },
+    onSuccess: async (_data) => {
+      await queryClient.invalidateQueries({ queryKey: [GET_ALL_USERS_FOR_ADMIN_KEY] });
+    }
+  });
+
+  return {
+    resetPassword: mutation.mutate,
+    isPending: mutation.isPending,
+    isError: mutation.isError
+  };
+};
+
+export const useChangePassword = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<void, Error, { user_id: number; oldPass: string; newPass: string }>({
+    mutationFn: async (data) => {
+      await changePassword(data);
+    },
+    onSuccess: async (_data) => {
+      await queryClient.invalidateQueries({ queryKey: [MY_INFO_KEY] });
+    }
+  });
+
+  return {
+    changePassword: mutation.mutate,
     isPending: mutation.isPending,
     isError: mutation.isError
   };
